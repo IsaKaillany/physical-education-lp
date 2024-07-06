7769802de82b138af5e586fa48480cb1fa71f11c
Isa Kaillany <isakaillany123@gmail.com>
2024-07-05 22:06:18 -0300

    chore: applied responsiveness

--- physical-education/src/components/header.jsx
+++ physical-education/src/components/header.jsx
@@ -2,9 +2,9 @@ import titleLogo from "../assets/titleLogo.png";
 
 export function Header() {
     return (
-        <nav className="flex items-center justify-between gap-4 max-w-full text-white">
-            <img src={titleLogo} className="" />
-            <div className="flex gap-8 justify-center items-center bg-[#0C1221] w-[480px] h-16 rounded-full p-5">
+        <nav className="flex items-center justify-between gap-4 text-white">
+            <img src={titleLogo} className="max-sm:size-1/2" />
+            <div className="flex gap-8 justify-center items-center bg-[#0C1221] w-[480px] h-16 rounded-full p-5 max-md:hidden">
                 <a className="cursor-pointer hover:text-[#0284C7]">Home</a>
                 <a className="cursor-pointer hover:text-[#0284C7]">About</a>
                 <a className="cursor-pointer hover:text-[#0284C7]">App</a>
