1e3fd2dda561dd02273074dd271522cccb467ab1
Isa Kaillany <isakaillany123@gmail.com>
2024-07-12 20:30:47 -0300

    chore: create cards and install libs for icons

---
+++ physical-education/src/components/cards.jsx
@@ -0,0 +1,30 @@
+import PropTypes from "prop-types";
+
+export default function CardInfo({ icon: Icon, iconSize, title, content }) {
+    const formattedContent = content
+        .split(/(Senior TechFit)/gi)
+        .map((part, index) =>
+            part.toLowerCase() === "senior techfit" ? (
+                <strong key={index}>{part}</strong>
+            ) : (
+                part
+            )
+        );
+
+    return (
+        <div className="bg-transparent w-80 h-96 flex flex-col items-center px-4 pt-5 rounded-xl ring ring-[#21325A] ring-inset">
+            <Icon className={`size-${iconSize} text-white`} />
+            <h1 className="text-[#EA9F0C] font-semibold mt-2 text-xl uppercase">
+                {title}
+            </h1>
+            <p className="text-white text-center mt-6">{formattedContent}</p>
+        </div>
+    );
+}
+
+CardInfo.propTypes = {
+    icon: PropTypes.elementType.isRequired,
+    iconSize: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+};
