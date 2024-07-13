df62d6c22d7805df1c0259076f7032f207551276
Isa Kaillany <isakaillany123@gmail.com>
2024-07-12 23:38:16 -0300

    chore: applying transform

--- physical-education/src/components/cards.jsx
+++ physical-education/src/components/cards.jsx
@@ -12,7 +12,7 @@ export default function CardInfo({ icon: Icon, iconSize, title, content }) {
         );
 
     return (
-        <div className="bg-transparent w-80 h-96 flex flex-col items-center px-4 pt-5 rounded-xl ring ring-[#21325A] ring-inset">
+        <div className="bg-white/[0.03] w-80 h-96 flex flex-col items-center px-4 pt-5 rounded-xl ring ring-[#21325A] ring-inset shadow-2xl shadow-black mx-4 transition-transform duration-300 ease-in-out hover:-translate-y-1 hover:rotate-1 hover:scale-105 transform perspective-1000 hover:rotate-x-6 hover:rotate-y-6">
             <Icon className={`size-${iconSize} text-white`} />
             <h1 className="text-[#EA9F0C] font-semibold mt-2 text-xl uppercase">
                 {title}
