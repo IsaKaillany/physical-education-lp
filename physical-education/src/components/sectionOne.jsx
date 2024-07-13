225b0cfceb927d3e45df7016e8bb243f0f777f38
Isa Kaillany <isakaillany123@gmail.com>
2024-07-12 22:24:27 -0300

    chore: update in small screens

--- physical-education/src/components/sectionOne.jsx
+++ physical-education/src/components/sectionOne.jsx
@@ -2,7 +2,7 @@ import { Header } from "./header";
 
 export default function SectionOne() {
     return (
-        <section className="md:bg-[url('./assets/bg-seniors.png')] bg-slate-700 min-w-full min-h-screen bg-cover bg-no-repeat py-4">
+        <section id="#" className="sm:bg-[url('./assets/bg-seniors.png')] bg-gradient-to-b from-[#16223D] to-gray-950 min-w-full  h-[800px] bg-cover bg-no-repeat py-4">
             <Header />
             <div className="mx-14 max-sm:mx-8">
                 <section className="text-white w-[580px] mt-36 text-justify max-sm:w-full">
