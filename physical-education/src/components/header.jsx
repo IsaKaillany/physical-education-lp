6982cebcc279774fb4659f29ac467a3021316878
Isa Kaillany <isakaillany123@gmail.com>
2024-07-12 23:38:49 -0300

    chore: update download button

--- physical-education/src/components/header.jsx
+++ physical-education/src/components/header.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import titleLogo from "../assets/titleLogo.png";
+import { MdOutlineFileDownload } from "react-icons/md";
 
 export function Header() {
     const [isScrolled, setIsScrolled] = useState(false);
@@ -21,13 +22,27 @@ export function Header() {
         >
             <img src={titleLogo} className="max-sm:size-1/2 my-1" />
             <div className="flex gap-8 justify-center items-center bg-[#0C1221] w-[480px] h-16 rounded-full p-5 max-md:hidden">
-                <a href="#" className="cursor-pointer hover:text-[#EA9F0C]">Home</a>
-                <a href="#about" className="cursor-pointer hover:text-[#EA9F0C]">About</a>
-                <a href="#app" className="cursor-pointer hover:text-[#EA9F0C]">App</a>
-                <a href="#contact" className="cursor-pointer hover:text-[#EA9F0C]">Contact</a>
+                <a href="#" className="cursor-pointer hover:text-[#EA9F0C]">
+                    Home
+                </a>
+                <a
+                    href="#about"
+                    className="cursor-pointer hover:text-[#EA9F0C]"
+                >
+                    About
+                </a>
+                <a href="#app" className="cursor-pointer hover:text-[#EA9F0C]">
+                    App
+                </a>
+                <a
+                    href="#contact"
+                    className="cursor-pointer hover:text-[#EA9F0C]"
+                >
+                    Contact
+                </a>
             </div>
-            <button className="w-36 h-16 rounded-full bg-[#0C1221] flex justify-center items-center p-4 hover:text-[#EA9F0C]">
-                Download App
+            <button className="w-36 h-16 rounded-full bg-[#0C1221] flex justify-center items-center gap-2 p-4 hover:text-[#EA9F0C]">
+                Download <MdOutlineFileDownload className="size-6" />
             </button>
         </nav>
     );
